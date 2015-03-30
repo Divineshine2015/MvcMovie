f47b353883464c7ae99d8639c2f028ff01b04eec
Jon Reed <mr.is@outlook.com>
2015-03-30 13:51:31 -0400

    Added unit test

---
+++ MvcMovie/MvcMovieTest.cs
@@ -0,0 +1,25 @@
+﻿using MvcMovie.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Xunit;
+
+namespace MvcMovie
+{
+    public class MvcMovieTest
+    {
+        [Fact]
+
+        public void SomeTest()
+        {
+            var controller = new HomeController();
+            var someView = controller.Index();
+            var theView = (ViewResult)someView;
+            dynamic model = theView.Model;
+
+            Assert.NotNull(theView);
+        }
+    }
+}
